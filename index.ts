205debb80f1c40ab23a8e12acfd34a83d6f076ca
Mauricio <mauricio.palma@sinnerschrader.com>
2018-09-25 08:40:51 +0200

    feat(vector): create the vector factory

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
-import {foo} from './src/vector';
+import { Vector } from './src/vector';
 
 
-console.log(`Hello world, ${foo}`);
+const vector1 = Vector({ x: 10, y: 5, z: 4 }).create();
+const vector2 = Vector({ x: 10, y: 5, z: 4 }).create();
+// const vector3 = vector1.add(vector2);
+console.log('the cureent vector was created', vector1, vector2, '&&&');
--- src/vector.ts
+++ src/vector.ts
@@ -1,1 +1,28 @@
-export const foo = 'blah';
+export interface VectorProps {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface VectorFactoryProps {
+  create(): VectorProps;
+  add(): VectorProps;
+}
+
+
+export const Vector = ({ x, y, z }: VectorProps) => ({
+  create: (): VectorFactoryProps => ({
+    x,
+    y,
+    z,
+    add: ({ x2, y2, z2 }): VectorProps => (
+      Object.create({
+        x: x2 + x,
+        y: y2 + y,
+        z: z2 + z
+      })
+    )
+  }),
+});
+
+export default Vector;
